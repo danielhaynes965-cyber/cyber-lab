0fde29e52a6f95e8efc3ffb267b286253bf4f62d
danielhaynes965-cyber <daniel.haynes965@gmail.com>
2026-07-02 22:55:28 +0800

    debug script

--- ctf/forensics/sandbox/rsa_even_n_decrypt.py
+++ ctf/forensics/sandbox/rsa_even_n_decrypt.py
@@ -5,13 +5,13 @@
 
 import sys
 
-N = sys-argv[1]
+N = int(sys.argv[1])
 e = 65537
 q = N // 2
 phi_N = q - 1
 d = pow(e, -1, phi_N)
   
-c = sys.argv[2]
+c = int(sys.argv[2])
 m = pow(c, d, N)
 flag = m.to_bytes(29)
 print(flag)
